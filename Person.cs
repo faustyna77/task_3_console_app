8d959e99c2665e57394cdafd6f5daf3dcf06f98b
Faustyna Misiura <110495453+faustyna77@users.noreply.github.com>
2024-01-07 21:23:54 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_console_app
+{
+    internal class Person
+    {
+
+        string name;
+        string nazwisko;
+        string miejsce;
+        int year;
+        int month;
+        int day;
+        int wiek;
+        int months;
+        int days;
+        public Person(string name,string nazwisko,string miejsce,int year,int month,int day)
+
+        {
+            this.name = name;
+            this.nazwisko = nazwisko;
+            this.miejsce = miejsce;
+            this.year = year;
+            this.month = month;
+            this.day = day;
+
+        }
+
+        
+
+        public void Age()
+        {
+            days = DateTime.Now.Day - this.day;
+            months = DateTime.Now.Month - this.month;
+            wiek = DateTime.Now.Year - this.year;
+            if (months<0 ||days<0)
+            {
+               wiek= wiek - 1;
+            }
+            
+           
+            Console.WriteLine("wiek to " + wiek + " "+"lat");
+        }
+        public void PersonMethod()
+        {
+            Console.WriteLine($"Imie to {name} nazwisko to {nazwisko} miejsce urodzin {miejsce}");
+            Age();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_console_app
+{
+    class Program
+    {
+       
+        public static void Main()
+        {
+            Console.WriteLine("podaj imie");
+            string imie = Console.ReadLine();
+            Console.WriteLine("podaj nazwisko");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine("podaj miejsce");
+            string miejsce = Console.ReadLine();
+            Console.WriteLine("podaj rok");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj miesiac ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj dzień ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Person person = new Person(imie, nazwisko,miejsce,year,month,day);
+          
+            person.PersonMethod();
+        }
+
+    }
+}
